255abe4c02017bfee3e88551d60813fb0589d358
Oleksii Maliarov <lex@debitoor.com>
2017-08-31 18:54:34 +0200

    websockets

--- src/app.js
+++ src/app.js
@@ -3,13 +3,15 @@ const express = require('express');
 const alexa = require('alexa-app');
 
 const PORT = process.env.PORT || 8080;
-const app = express();
+const expressApp = express();
+
 
 // ALWAYS setup the alexa app and attach it to express before anything else.
 const alexaApp = new alexa.app('amzn1.ask.skill.4a5bae8a-ef54-41a0-94c5-df320e2c5352');
+const expressWs = require('express-ws')(expressApp);
 
 alexaApp.express({
-  expressApp: app,
+  expressApp: expressApp,
   //router: express.Router(),
 
   endpoint: '/echo',
@@ -28,18 +30,30 @@ alexaApp.launch((req, res) => {
   res.say('Welcome to Debitoor App!');
 });
 
+/*
 alexaApp.intent('getUnpaidInvoicesCount', {
-    'slots': { },
-    'utterances': [
-        'get unpaid invoices count',
-        'unpaid invoices count',
-        'for unpaid invoices count',
-        'how many unpaid invoices do I have'
-     ]
-  },
+  'slots': {},
+  'utterances': [
+    'get unpaid invoices count',
+    'unpaid invoices count',
+    'for unpaid invoices count',
+    'how many unpaid invoices do I have'
+  ]
+},
   (req, res) => {
     res.say('You have one unpaid invoice!');
   }
 );
+*/
 
-app.listen(PORT, () => console.log('Listening on port ' + PORT + '.'));
+require('./commands/openProjects').use({expressApp, alexaApp, expressWs});
+
+expressApp.ws('/commands', (ws, req) => {
+  ws.on('message', (msg) => {
+    ws.send({command:'ping'});
+    console.log(msg);
+  });
+});
+
+
+expressApp.listen(PORT, () => console.log('Listening on port ' + PORT + '.'));
